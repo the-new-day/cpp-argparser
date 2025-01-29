eda9077ee01a21611775d6fdf62bfca1c915b9c0
the-new-day <balakineg977@gmail.com>
2025-01-29 23:55:23 +0300

    Updated main.cpp example

--- bin/main.cpp
+++ bin/main.cpp
@@ -13,31 +13,38 @@ int main(int argc, char** argv) {
     Options opt;
     std::vector<int> values;
 
-    ArgumentParser::ArgParser parser("Program");
-    parser.AddIntArgument("N").MultiValue(1).Positional().StoreValues(values);
-    parser.AddFlag("sum", "add args").StoreValue(opt.sum);
-    parser.AddFlag("mult", "multiply args").StoreValue(opt.mult);
-    parser.AddHelp('h', "help", "Program accumulate arguments");
+    ArgumentParser::ArgParser parser("Program", "Program accumulate arguments");
+    parser.AddArgument<int>("numbers").MultiValue(1).Positional().StoreValues(values);
+    parser.AddFlag('s', "sum", "Sum arguments").StoreValue(opt.sum);
+    parser.AddFlag('m', "mult", "Multiply arguments").StoreValue(opt.mult);
+    parser.AddArgument<std::string>('n', "name", "Your name").Default("John Doe");
+    parser.AddHelp('h', "help", "Show help and exit");
 
-    if(!parser.Parse(argc, argv)) {
+    if (!parser.Parse(argc, argv)) {
         std::cout << "Wrong argument" << std::endl;
         std::cout << parser.HelpDescription() << std::endl;
         return 1;
     }
 
-    if(parser.Help()) {
+    if (parser.Help()) {
         std::cout << parser.HelpDescription() << std::endl;
         return 0;
     }
 
-    if(opt.sum) {
-        std::cout << "Result: " << std::accumulate(values.begin(), values.end(), 0) << std::endl;
-    } else if(opt.mult) {
-        std::cout << "Result: " << std::accumulate(values.begin(), values.end(), 1, std::multiplies<int>()) << std::endl;
-    } else {
+    if (!opt.sum && !opt.mult) {
         std::cout << "No options have been chosen" << std::endl;
         std::cout << parser.HelpDescription();
         return 1;
+    }
+
+    std::cout << "Hello " << *parser.GetValue<std::string>("name") << '!' << std::endl;
+
+    if (opt.sum) {
+        std::cout << "Sum: " << std::accumulate(values.begin(), values.end(), 0) << std::endl;
+    }
+    
+    if (opt.mult) {
+        std::cout << "Product: " << std::accumulate(values.begin(), values.end(), 1, std::multiplies<int>()) << std::endl;
     }
 
     return 0;
